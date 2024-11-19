8216f7aeb8f5236269a0e831bbef1c67cb4cfe73
rjs7273 <rjs7273@gmail.com>
2024-11-19 10:13:11 +0900

    Fetch_All_Data python 버전으로 변환 완료
    필요없는 파일들 제거
    차후 Fetch_Stock_Code를 .py 파일로 바꾸고, 파일에 내재한 버그를 제거할 예정임.

--- Fetch_All_Data.py
+++ Fetch_All_Data.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import time
 import random
 import sys
+import re
 
 class TickerDataCollector:
     def __init__(self, tickers_file='tickers.csv',results_file='financial_results.csv'):
@@ -35,28 +36,34 @@ class TickerDataCollector:
     def load_results(self):
         """이전에 저장된 결과 파일을 로드하는 메서드"""
         if os.path.exists(self.results_file):
-            self.results_df = pd.read_csv(self.results_file)
+            self.results_df = pd.read_csv(self.results_file,index_col=0)
         else:
             print(f"결과 파일 '{self.results_file}'을 찾을 수 없습니다.")
-            self.results_df = self.results_df.copy(deep=True) # 결과 데이터프레임에 티커 추가,
+            self.results_df = self.tickers_df.copy(deep=True) # 결과 데이터프레임에 티커 추가
             self.results_df.set_index('Tickers', inplace=True) # 티커명을 인덱스로 설정
-            self.results_df.columns = [
-                'Index', 'Market Cap', 'Income', 'Sales', 'Book/sh', 'Cash/sh', 
-                'Dividend Est.', 'Dividend TTM', 'Dividend Ex-Date', 'Employees', 
-                'Option/Short', 'Sales Surprise', 'SMA20', 'P/E', 'Forward P/E', 
-                'PEG', 'P/S', 'P/B', 'P/C', 'P/FCF', 'Quick Ratio', 'Current Ratio', 
-                'Debt/Eq', 'LT Debt/Eq', 'EPS Surprise', 'SMA50', 'EPS (ttm)', 
-                'EPS next Y', 'EPS next Q', 'EPS this Y', 'EPS next 5Y', 'EPS past 5Y', 
-                'Sales past 5Y', 'EPS Y/Y TTM', 'Sales Y/Y TTM', 'EPS Q/Q', 'Sales Q/Q', 
-                'SMA200', 'Insider Own', 'Insider Trans', 'Inst Own', 'Inst Trans', 
-                'ROA', 'ROE', 'ROI', 'Gross Margin', 'Oper. Margin', 'Profit Margin', 
-                'Payout', 'Earnings', 'Trades', 'Shs Outstand', 'Shs Float', 
-                'Short Float', 'Short Ratio', 'Short Interest', '52W Range', 
-                '52W High', '52W Low', 'RSI (14)', 'Recom', 'Rel Volume', 
-                'Avg Volume', 'Volume', 'Perf Week', 'Perf Month', 'Perf Quarter', 
-                'Perf Half Y', 'Perf Year', 'Perf YTD', 'Beta', 'ATR (14)', 
-                'Volatility', 'Target Price', 'Prev Close', 'Price', 'Change'
-            ] # 데이터 열을 추가.
+            # print(f"self.results_df.index : {self.results_df.index}")
+            # self.results_df.to_csv('test1.csv', index=True)
+            new_columns = [
+                'Index', 'Market Cap', 'Income', 'Sales', 'Book/sh', 'Cash/sh', 'Dividend Est.',
+                'Dividend TTM', 'Dividend Ex-Date', 'Employees', 'Option/Short', 'Sales Surprise',
+                'SMA20', 'P/E', 'Forward P/E', 'PEG', 'P/S', 'P/B', 'P/C', 'P/FCF',
+                'Quick Ratio', 'Current Ratio', 'Debt/Eq', 'LT Debt/Eq', 'EPS Surprise', 'SMA50',
+                'EPS (ttm)', 'EPS next Y', 'EPS next Q', 'EPS this Y', 'EPS Growth next Y', 'EPS next 5Y',
+                'EPS past 5Y', 'Sales past 5Y', 'EPS Y/Y TTM', 'Sales Y/Y TTM', 'EPS Q/Q', 'Sales Q/Q',
+                'SMA200', 'Insider Own', 'Insider Trans', 'Inst Own', 'Inst Trans', 'ROA', 'ROE',
+                'ROI', 'Gross Margin', 'Oper. Margin', 'Profit Margin', 'Payout', 'Earnings', 'Trades',
+                'Shs Outstand', 'Shs Float', 'Short Float', 'Short Ratio', 'Short Interest', '52W Range',
+                '52W High', '52W Low', 'RSI (14)', 'Recom', 'Rel Volume', 'Avg Volume', 'Volume',
+                'Perf Week', 'Perf Month', 'Perf Quarter', 'Perf Half Y', 'Perf Year', 'Perf YTD',
+                'Beta', 'ATR (14)', 'Volatility', 'Target Price', 'Prev Close', 'Price', 'Change'
+            ]
+            for col in new_columns:
+                self.results_df[col] = pd.NA
+            # self.results_df.to_csv("test.csv", index=True)
+            # print(f"self.results_df.shape : {self.results_df.shape}")
+            # print(f"self.results_df.columns : {self.results_df.columns}")
+            # print(f"len(self.results_df) : {self.results_df}")
+            # print(f"self.results_df.colums : {self.results_df.columns}")
 
     def fetch_data(self,ticker):
         """웹 페이지를 요청하는 메서드.
@@ -75,32 +82,73 @@ class TickerDataCollector:
             print("너무 많은 요청.. 재시도 중")
             time.sleep(random.uniform(3, 5))
             response = requests.get(url, headers=headers)
+        response_data = response.text
+        if response.status_code != 200:
+            print(f"오류 발생: 상태 코드 {response.status_code}")
+            sys.exit()  # 프로그램 종료
+        # print(f"response_status_code : {response.status_code}")
+        # print(f"response : {response_data}")
+        with open("output.txt", "w", encoding="utf-8") as file:
+            file.write(response_data)
         return BeautifulSoup(response.text, "html.parser")
 
-    def parse_table(self, html_data):
-        """파싱한 html 테이블에서 특정 속성을 이용해 데이터를 추출하자. """
-        # 실제 데이터 파싱 로직은 나중에 구현(BeautifulSoup 사용)
-        # print(f"파싱 중 : {html_data}")
-        return ["Parsed Data"] # 더미 파싱 결과 (실제 파싱 로직으로 대체)
+    def parse_table(self, soup):
+        # print(f"soup : {soup.get_text()}") # soup는 제대로 출력됨
+        # soup_data = soup.get_text()
+        # print(f"type(soup_data) : {type(soup_data)}")
+        # with open("output.txt", "w", encoding="utf-8") as file:
+        #     file.write(soup_data)
+        table = soup.find("table", {"class" : "snapshot-table2"})
+        # print(f"table : {table}") # 빈 테이블이 나옴
+        rows = table.find_all("tr")
+        table_data = []
 
+        # print(f"rows : {rows}")
+
+        for row in rows:
+            cells = row.find_all("td")  # 각 행의 모든 셀 가져오기
+            row_data = [cell.get_text(strip=True) for cell in cells]  # 텍스트 추출 및 공백 제거
+            table_data.append(row_data)  # 추출한 데이터 리스트에 추가
+
+        # 데이터프레임으로 변환
+        df = pd.DataFrame(table_data)
+        value_list = []
+        index_list = []
+        for i in range(0,12,2): # 필요한 데이터만 추출
+            # index_list.extend(df[i].tolist())
+            value_list.extend(df[i + 1].tolist()) # 짝수 열의 데이터만 추가
+        # print(f"index_list : {index_list}")
+        # print(f"value_list : {value_list}")
+        return value_list
+
+    # 모든 티커에 대해 데이터를 수집하는 메서드
     def collect_data(self):
-        """모든 티커에 대해 데이터를 수집하는 메서드.
-        for문을 이용해 기존에 탐색했던 구간부터, 문서의 끝까지 탐색해야 한다."""
-        for ticker in self.results_df.index[:10]: # 임시로 10개의 티커만 탐색하도록 지정
-            html_data = self.fetch_data(ticker)
-            parsed_data = self.parse_table(html_data)
-            self.results_df.loc[ticker] = parsed_data
-            if len(self.results_df) % 10 == 0:
-                self.save_results()
+        start_index = self.results_df['Market Cap'].count() # 이미 수집된 데이터 개수
+        print(f"start_index : {start_index}")
+        # start_ticker = self.results_df.index.get_loc(start_index)
+
+        for ticker in self.results_df.index[start_index:]:
+            print(f"현재 탐색 티커: {ticker}")
+            soup = self.fetch_data(ticker)  # 웹 페이지 데이터 가져오기
+            # print(f"soup : {soup}")
+            value_list = self.parse_table(soup)  # 테이블 데이터 파싱
+            # print(f"value_list : {value_list}")
+            # print(f"len(value_list) : {len(value_list)}")
+            # print(f"len(self.results_df.loc[ticker]) : {len(self.results_df.loc[ticker])}")
+            self.results_df.loc[ticker] = value_list  # 데이터프레임에 추가
+            self.save_results()  # 결과 저장
+
     def save_results(self):
         """수집된 데이터를 CSV 파일로 저장하는 메서드.
         메모리 과부화를 막기 위해 10개의 티커마다 저장하도록 기능하면 좋겠다."""
-        print(f"결과를 저장하는 중 : {self.results_file}")
-        self.results_df.to_csv(self.results_file, index=False)
+        if self.results_df['Market Cap'].count() % 10 == 0:
+            print(f"결과를 저장하는 중 : {self.results_file}")
+            self.results_df.to_csv(self.results_file, index=True)
 
 if __name__ == "__main__":
     tickers_file = 'tickers.csv' # 티커 파일 경로
     financial_results_file = 'financial_results.csv' # 결과 파일 경로
 
     collector = TickerDataCollector(tickers_file, financial_results_file) # 객체 생성
+    # print(f"self.results_df.index : {collector.results_df.index[:10]}")
     collector.collect_data() # 데이터 수집 시작
